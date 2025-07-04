95221755c756e5987ccd92db34e75322b93c70a1
Erik <erikgavs@gmail.com>
2025-07-05 01:23:25 +0200

    acabadas las animaciiones de las bebidas y ahora tocando js

--- Web/js/script.js
+++ Web/js/script.js
@@ -1,9 +1,11 @@
 
-// Más adelante hacer scroll lento
+// SCROLL DE LA PÁGINA PRINCIPAL
 function scrollToSection() {
     document.getElementById('ancla').scrollIntoView({
         behavior: "smooth"
     })
 }
 
-
+document.querySelector(".card").addEventListener("click", function(){
+    window.location.href = "bebida1.html";
+});
