fbfcfb6543c2742928e773700461497bf22779af
Erik <erikgavs@gmail.com>
2025-07-09 00:57:42 +0200

    acabada página de login y enlaces arreglados

--- Web/flask/app.py
+++ Web/flask/app.py
@@ -24,6 +24,10 @@ def members():
 def login():
      return render_template('login.html')
 
+@app.route('/register')
+def register():
+     return render_template('register.html')
+
 # FORMULARIO 
 @app.route('/suscribirse', methods=['POST'])
 def suscribirse():
