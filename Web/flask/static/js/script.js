c4ce5362d4b06d53630cecff2a1d494cbc76ded4
Erik <erikgavs@gmail.com>
2025-07-25 01:23:00 +0200

    acabado el formulario de login y acabando el de register y añadido código js

--- Web/flask/static/js/script.js
+++ Web/flask/static/js/script.js
@@ -5,3 +5,21 @@ function scrollToSection() {
         behavior: "smooth"
     })
 }
+
+// COMPROBACIÓN DE QUE LAS DOS CONTRASEÑAS DEL FORM (REGISTER) ES CORRECTA
+
+const form = document.querySelector('.formulario-register');
+const password1 = document.querySelector('.input-password1');
+const password2 = document.querySelector('.input-password2');
+const mensajeError = document.querySelector('.mensaje-error');
+
+form.addEventListener('submit', function(event) {
+    if (password1.value !== password2.value) {
+        event.preventDefault();
+        mensajeError.textContent = "Las contraseñas no coinciden.";
+    } else {
+        mensajeError.textContent = "";
+    }
+});
+
+// REVISAR APARTADO Y ENCARGARME DE ENTENDERLO BIEN BIEN
